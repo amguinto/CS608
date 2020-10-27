412b4497ae90cb7c123c38038be4cb552ed0e64e
Andy Guinto <aguinto@localhost.localdomain>
2020-10-27 14:48:04 -0400

    Added support for adding block sizes for numeric representation

--- test/test_Message_utils.cpp
+++ test/test_Message_utils.cpp
@@ -6,33 +6,125 @@
 
 TEST(test_Message_utils, naive_plaintext_numeric)
 {
-    auto naive = message::naive_plaintext_numeric("njit");
-    EXPECT_EQ(naive[0], 13);
-    EXPECT_EQ(naive[1], 9);
-    EXPECT_EQ(naive[2], 8);
-    EXPECT_EQ(naive[3], 19);
+    // Old version.
+    auto naive_old = message::naive_plaintext_numeric("njit");
+    EXPECT_EQ(naive_old[0], 14);
+    EXPECT_EQ(naive_old[1], 10);
+    EXPECT_EQ(naive_old[2], 9);
+    EXPECT_EQ(naive_old[3], 20);
+    
+    auto naive = message::naive_plaintext_numeric("njit", 4);
+
+    // Block size of 4.
+    EXPECT_EQ(naive.size(), 1);    // Number of blocks
+    EXPECT_EQ(naive[0].size(), 4); // Number of characters per block
+
+    EXPECT_EQ(naive[0][0], 14); // n
+    EXPECT_EQ(naive[0][1], 10); // j
+    EXPECT_EQ(naive[0][2], 9);  // i
+    EXPECT_EQ(naive[0][3], 20); // t
+
+    // Block size of 1.
+    auto naive1 = message::naive_plaintext_numeric("njit", 1);
+    EXPECT_EQ(naive1.size(), 4);    // Number of blocks
+    EXPECT_EQ(naive1[0].size(), 1); // Number of characters per block
+
+    EXPECT_EQ(naive1[0][0], 14); // n
+    EXPECT_EQ(naive1[1][0], 10); // j
+    EXPECT_EQ(naive1[2][0], 9);  // i
+    EXPECT_EQ(naive1[3][0], 20); // t
+
+    // Block size of 10, we would have 0 padding.
+    auto naive2 = message::naive_plaintext_numeric("njit", 10);
+    EXPECT_EQ(naive2.size(), 1);    // Number of blocks
+    EXPECT_EQ(naive2[0].size(), 10); // Number of characters per block
+    EXPECT_EQ(naive2[0][0], 14); // n
+    EXPECT_EQ(naive2[0][1], 10); // j
+    EXPECT_EQ(naive2[0][2], 9);  // i
+    EXPECT_EQ(naive2[0][3], 20); // t
+    EXPECT_EQ(naive2[0][4], 0);  // 0
+    EXPECT_EQ(naive2[0][5], 0);  // 0
+    EXPECT_EQ(naive2[0][6], 0);  // 0
+    EXPECT_EQ(naive2[0][7], 0);  // 0
+    EXPECT_EQ(naive2[0][8], 0);  // 0
+    EXPECT_EQ(naive2[0][9], 0);  // 0
+    // for (const auto& block : naive2)
+    // {
+    //     std::cout << "block = ";
+    //     for (const auto& c : block)
+    //     {
+    //         std::cout << c << "\t";
+    //     }
+    //     std::cout << std::endl;
+    // }
 }
 
 TEST(test_Message_utils, encode_naive_representation)
 {
-    auto naive = message::naive_plaintext_numeric("njit");
+    auto naive = message::naive_plaintext_numeric("njit", 4);
     auto compressed = message::encode_naive_representation(naive);
-    // NOTE: This changes depending on how large the numeric map is. If 25, = 234799
-    EXPECT_EQ(compressed, 262675);
+    EXPECT_EQ(compressed.size(), 1);
+    EXPECT_EQ(compressed[0], 315440);
+
+
+    auto naive1 = message::naive_plaintext_numeric("njit", 1);
+    auto compressed1 = message::encode_naive_representation(naive1);
+    // Can't compress if only 1 character, so it's itself.
+    EXPECT_EQ(compressed1.size(), 4);
+    EXPECT_EQ(compressed1[0], 14);
+    EXPECT_EQ(compressed1[1], 10);
+    EXPECT_EQ(compressed1[2], 9);
+    EXPECT_EQ(compressed1[3], 20);
+
+    // Zero padding example.
+    auto naive2 = message::naive_plaintext_numeric("njit", 10);
+    auto compressed2 = message::encode_naive_representation(naive2);
+    EXPECT_EQ(compressed2.size(), 1);
+    EXPECT_EQ(compressed2[0], 315440);
 }
 
 TEST(test_Message_utils, decode_naive_representation)
 {
-    // NOTE: This changes depending on how large the numeric map is. If 25, = 234799
-    std::string decoded = message::decode_naive_representation(262675);
+    // Deprecated version
+    std::string decoded = message::decode_naive_representation(315440);
     EXPECT_EQ(decoded, "njit");
+
+
+    std::vector<mpz_class> compressed;
+    mpz_class block(315440);
+    compressed.emplace_back(block);
+    std::string message = message::decode_naive_representation(compressed);
+    EXPECT_EQ(message, "njit");
+
+
+    // 1 character per block.
+    auto numeric_blocks = message::naive_plaintext_numeric("njit", 1);
+    auto compressed_blocks = message::encode_naive_representation(numeric_blocks);
+    std::string message2 = message::decode_naive_representation(compressed_blocks);
+    EXPECT_EQ(message2, "njit");
 }
 
 TEST(test_Message_utils, long_message)
 {
     std::string str = "do not send files as attachments to emails";
-    auto naive = message::naive_plaintext_numeric(str);
+    auto naive_old = message::naive_plaintext_numeric(str);
+    auto compressed_old = message::encode_naive_representation(naive_old);
+    std::string decoded_old = message::decode_naive_representation(compressed_old);
+    EXPECT_EQ(str, decoded_old);
+
+    auto naive = message::naive_plaintext_numeric(str, 2);
     auto compressed = message::encode_naive_representation(naive);
     std::string decoded = message::decode_naive_representation(compressed);
     EXPECT_EQ(str, decoded);
+
 }
+
+// TEST(test_Message_utils, practice_test)
+// {
+//     std::string str = "Do not send files as attachments to eMails";
+//     auto naive = message::naive_plaintext_numeric(str);
+//     auto compressed = message::encode_naive_representation(naive);
+//     std::string decoded = message::decode_naive_representation(compressed);
+//     EXPECT_EQ(str, decoded);
+//     std::cout << decoded << std::endl;
+// }
--- utils/Message_utils.hpp
+++ utils/Message_utils.hpp
@@ -15,14 +15,34 @@
 namespace message
 {
 static const std::map<char, std::size_t> numeric_plaintext_map = {
-    { 'a', 0 },  { 'b', 1  }, { 'c', 2 },  { 'd', 3  }, 
-    { 'e', 4 },  { 'f', 5  }, { 'g', 6 },  { 'h', 7  }, 
-    { 'i', 8 },  { 'j', 9  }, { 'k', 10 }, { 'l', 11 }, 
-    { 'm', 12 }, { 'n', 13 }, { 'o', 14 }, { 'p', 15 }, 
-    { 'q', 16 }, { 'r', 17 }, { 's', 18 }, { 't', 19 }, 
-    { 'u', 20 }, { 'v', 21 }, { 'w', 22 }, { 'x', 23 },
-    { 'y', 24 }, { 'z', 25 }, { ' ', 26}
+    { 'a', 1 },  { 'b', 2  }, { 'c', 3 },  { 'd', 4  }, 
+    { 'e', 5 },  { 'f', 6  }, { 'g', 7 },  { 'h', 8  }, 
+    { 'i', 9 },  { 'j', 10 }, { 'k', 11 }, { 'l', 12 }, 
+    { 'm', 13 }, { 'n', 14 }, { 'o', 15 }, { 'p', 16 }, 
+    { 'q', 17 }, { 'r', 18 }, { 's', 19 }, { 't', 20 }, 
+    { 'u', 21 }, { 'v', 22 }, { 'w', 23 }, { 'x', 24 },
+    { 'y', 25 }, { 'z', 26 }, { ' ', 27 }, { '\0',  0 }
 };
+// Example in textbook.
+// static const std::map<char, std::size_t> numeric_plaintext_map = {
+//     { 'a', 0 },  { 'b', 1  }, { 'c', 2 },  { 'd', 3  }, 
+//     { 'e', 4 },  { 'f', 5  }, { 'g', 6 },  { 'h', 7  }, 
+//     { 'i', 8 },  { 'j', 9  }, { 'k', 10 }, { 'l', 11 }, 
+//     { 'm', 12 }, { 'n', 13 }, { 'o', 14 }, { 'p', 15 }, 
+//     { 'q', 16 }, { 'r', 17 }, { 's', 18 }, { 't', 19 }, 
+//     { 'u', 20 }, { 'v', 21 }, { 'w', 22 }, { 'x', 23 },
+//     { 'y', 24 }, { 'z', 25 }, { ' ', 26 }
+// };
+
+// static const std::map<char, std::size_t> numeric_plaintext_map = {
+//     { 'A', 0 },  { 'B', 1  }, { 'C', 2 },  { 'D', 3  }, 
+//     { 'E', 4 },  { 'F', 5  }, { 'G', 6 },  { 'H', 7  }, 
+//     { 'I', 8 },  { 'J', 9  }, { 'K', 10 }, { 'L', 11 }, 
+//     { 'M', 12 }, { 'N', 13 }, { 'O', 14 }, { 'P', 15 }, 
+//     { 'Q', 16 }, { 'R', 17 }, { 'S', 18 }, { 'T', 19 }, 
+//     { 'U', 20 }, { 'V', 21 }, { 'W', 22 }, { 'X', 23 },
+//     { 'Y', 24 }, { 'Z', 25 }, { ' ', 26}
+// };
 
 // static const std::map<char, std::string> numeric_plaintext_map = {
 //     { 'a', "00" }, { 'b', "01" }, { 'c', "02" }, { 'd', "03" }, 
@@ -39,7 +59,7 @@ char_to_numeric(const char& charac)
 {
     std::size_t numeric = std::numeric_limits<std::size_t>::max();
 
-    const auto it = numeric_plaintext_map.find(charac);
+    const auto it = numeric_plaintext_map.find(std::tolower(charac));
     if (it != numeric_plaintext_map.cend())
     {
         numeric = it->second;
@@ -48,7 +68,7 @@ char_to_numeric(const char& charac)
     {
         std::cout << "Ignoring character: " << charac << std::endl;
     }
-    
+
     return numeric;
 }
 
@@ -71,6 +91,53 @@ numeric_to_char(const T num)
     return c;
 }
 
+static inline std::vector<std::vector<std::size_t>>
+naive_plaintext_numeric(const std::string& plaintext, std::size_t block_size)
+{
+    std::vector<std::vector<std::size_t>> all_blocks;
+    all_blocks.reserve(std::ceil(plaintext.size() / block_size));
+
+    std::vector<std::size_t> block;
+    block.reserve(block_size);
+
+    // i = index of string
+    // b = keep track of block
+    for (std::size_t i = 0, b = 0; i < plaintext.size(); ++i, ++b)
+    {
+        // Get numeric.
+        auto num = char_to_numeric(plaintext[i]);
+        block.emplace_back(num);
+        // std::cout << "placing into block: " << plaintext[i] << std::endl;
+        // Reached our block limit.
+        if (b >= block_size - 1)
+        {
+            // std::cout << "reached block limit" << std::endl;
+            // Reset block counter.
+            b = 0;
+
+            // std::cout << "clearing out block after adding character: " << plaintext[i] << std::endl;
+            all_blocks.emplace_back(block);
+            block.clear();
+        }
+    }
+
+    // We didn't clear out our block because it's missing some characters.
+    // Make sure our last block is padded. Add 0s.
+    if (!block.empty())
+    {
+        while (block.size() < block_size)
+        {
+            block.emplace_back(0);
+        }
+        // Place back into vector.
+        all_blocks.emplace_back(block);
+    }
+
+
+    return all_blocks;
+}
+
+// Deprecated. no block size.
 static inline std::vector<std::size_t> 
 naive_plaintext_numeric(const std::string& str)
 {
@@ -86,7 +153,54 @@ naive_plaintext_numeric(const std::string& str)
     return numeric_form;
 }
 
-static inline mpz_class 
+static inline std::vector<mpz_class>
+encode_naive_representation(const std::vector<std::vector<std::size_t>>& all_blocks)
+{
+    std::vector<mpz_class> all_blocks_compressed;
+    all_blocks_compressed.reserve(all_blocks.size());
+    // std::cout << all_blocks.size() << std::endl;
+    for (const auto& block : all_blocks)
+    {
+        mpz_class compressed_block{};
+        // std::cout << "current block: ";
+        std::size_t actual_block_size = block.size() - 1;
+
+        // Get an r value in case we zero padded, and r would be larger than it actually should be.
+        if (block.back() == 0)
+        {
+            for (std::size_t i = 0; i <= block.size() - 1; ++i)
+            {
+                if (block[i] == 0)
+                {
+                    // Need to go down because we don't want to include this character.
+                    actual_block_size = i - 1;
+                    // std::cout << "actual_block_size = " << actual_block_size << std::endl;
+                    break;
+                }
+            }
+        }
+        for (std::size_t i = 0, r = actual_block_size;
+            i <= actual_block_size; 
+            ++i, --r)
+        {
+            // std::cout << block[i] << "\t";
+            // r would be very large if we have a 0, so skip it.
+
+            mpz_class result = block[i] * math::IPow(numeric_plaintext_map.size(), r);
+            // std::cout << block[i] << "\t*\t" << numeric_plaintext_map.size() << "^" << r << std::endl;
+            // std::cout << "result = " << result << "\t";
+            compressed_block += result;
+        }
+        // std::cout << std::endl;
+        // Push compressed version to new vector.
+        all_blocks_compressed.emplace_back(compressed_block);
+    }
+
+    return all_blocks_compressed;
+}
+
+// Deprecated. no block size.
+static inline mpz_class
 encode_naive_representation(const std::vector<std::size_t>& numeric_form)
 {
     mpz_class compressed_num{};
@@ -101,6 +215,45 @@ encode_naive_representation(const std::vector<std::size_t>& numeric_form)
     return compressed_num;
 }
 
+static inline std::string 
+decode_naive_representation(std::vector<mpz_class>& all_blocks_compressed)
+{
+    std::string text = "";
+
+    // Starting from bottom up will always get the same order of characters 
+    // regardless of block size.
+    for (auto rit_block = all_blocks_compressed.crbegin();
+         rit_block != all_blocks_compressed.crend();
+         ++rit_block)
+    {
+        auto block_value = *rit_block;
+        // When we have more than 1 character per block.
+        while (block_value > numeric_plaintext_map.size())
+        {
+            mpz_class num{};
+            mpz_powm_ui(num.get_mpz_t(),
+                        block_value.get_mpz_t(),   // Base
+                        1, // Exponent is 1
+                        mpz_class(numeric_plaintext_map.size()).get_mpz_t());
+
+            text.insert(0, 1, numeric_to_char(num));
+            block_value = (block_value - num) / numeric_plaintext_map.size();
+        }
+
+        // Either we have 1 character per block or we're on the last character.
+        if (block_value < numeric_plaintext_map.size())
+        {
+            // std::cout << "less, block_compressed = " << block_value << std::endl;
+            // Most likely last letter.
+            text.insert(0, 1, numeric_to_char(block_value));
+            // std::cout << "text = " << text << std::endl;
+            // std::cout << std::endl;
+        }
+    }
+    return text;
+}
+
+// Deprecated. no block size.
 static inline std::string 
 decode_naive_representation(mpz_class compressed_num)
 {
@@ -120,6 +273,5 @@ decode_naive_representation(mpz_class compressed_num)
 
     return text;
 }
-
 } // namespace message
 #endif // MESSAGE_UTILS_HPP
